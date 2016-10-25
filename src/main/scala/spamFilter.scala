d7e5bffadb61f7fccba7cc86f0d6c130cf38edfe
Ignacio Uyá Lasarte <i.uya.lasarte@gmail.com>
2016-10-25 13:37:14 +0200

    Initial version of probaWordDir (incomplete)

--- src/main/scala/spamFilter.scala
+++ src/main/scala/spamFilter.scala
@@ -8,37 +8,35 @@ object spamFilter {
 
   def probaWordDir(sc:SparkContext)(filesDir:String) :(RDD[(String, Double)], Long) = {
     println("Call to probaWordDir with args" + filesDir)
-    // This function reads all the text files within a directory
     // wholeTextFiles get back a pair RDD where the key is the name of the input file.
-    val files  = sc.wholeTextFiles("/" + filesDir + "/*.txt").collect.toList.map(x => x._1)
+    val rdd = sc.wholeTextFiles(filesDir)
     // The number of files is counted and stored in a variable nbFiles
-    val nbFiles = files.size
+    val nbFiles = rdd.count()
+    // Non informative words must be removed from the set of unique words.
+    val stopWords = Set(".", ":", ",", " ", "/", "\\", "-", "'", "(", ")", "@")
     // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
-    // toSet is used to remove duplicate words. listOfWords is the set of unique words
-    var listOfWords = files.flatMap(x => (sc.textFile(x).flatMap(_.split("\\s+"))).collect.toSet.toList).toSet
-    // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
-    listOfWords = listOfWords - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
-    // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
-    def dfCalc(t:String, D: List[String]): Int = D.flatMap(x => sc.textFile(x).flatMap(_.split("\\s+")).filter(x => x==t).collect.toSet).count(x => x.size>0)
-    // map structure: word => number of occurrences (files) of this word
-    val probaWord :RDD[(String, Double)] =sc.parallelize(listOfWords.map(x => (x, (dfCalc(x, files) / nbFiles).toDouble)).toSeq)
+    val wordBagRdd: RDD[(String,Set[String])] = rdd.map(textTuple => (textTuple._1,textTuple._2.split(" ").toSet.diff(stopWords)))
+    // Get the Number of occurrences amongst all files
+    val wordCountRdd = wordBagRdd.flatMap(x => x._2.flatMap(x=> (x,1)))
+    val probaWord :RDD[(String, Double)] = null
+    //val probaWord :RDD[(String, Double)] =sc.parallelize(listOfWords.map(x => (x, (dfCalc(x, files) / nbFiles).toDouble)).toSeq)
     (probaWord, nbFiles)
   }
 
-  def computeMutualInformationFactor(
+  /*def computeMutualInformationFactor(
     probaWC:RDD[(String, Double)],
     probaW:RDD[(String, Double)],
     probaC: Double,
     probaDefault: Double // default value when a probability is missing
   ):RDD[(String, Double)] = {
   // Code to complete..
-  }
+  }*/
 
   // RDD with the map structure: word => probability the word occurs (or not) in an email of a given class.
-  def probaWordClass(word:String,clase:String,filesDir:String): (RDD[(String, Double)]) = {
+  //def probaWordClass(word:String,clase:String,filesDir:String): (RDD[(String, Double)]) = {
     // compute the probaOccurs for clase emails
 
-  }
+  //}
 
   def main(args: Array[String]) {
     if(args.length>0)
@@ -47,13 +45,14 @@ object spamFilter {
       println("You should provide a directory path ")
 
   // Code to complete...
-    //val conf = new SparkConf().setAppName("Spam Filter Application")
     val conf = new SparkConf().setMaster("local").setAppName("Spam Filter Application")
+    val fileDir = "hdfs://tmp/ling-spam"
     val sc = new SparkContext(conf)
-    val (probaW, nbFiles)= probaWordDir(sc)(args(0))
-    val probaWC // RDD with the map structure: word => probability the word occurs (or not) in an email of a given class.
-    val probaC  // the probability that an email belongs to the given class.
-    computeMutualInformationFactor(probaWC,probaW,probaC,10) // the last is a default value
+    val (probaW, nbFiles)= probaWordDir(sc)(fileDir)
+    print(probaW, nbFiles)
+    //val probaWC // RDD with the map structure: word => probability the word occurs (or not) in an email of a given class.
+    //val probaC  // the probability that an email belongs to the given class.
+    //computeMutualInformationFactor(probaWC,probaW,probaC,10) // the last is a default value
 
   }
 
