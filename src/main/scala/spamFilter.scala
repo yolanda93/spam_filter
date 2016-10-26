f46fd6f29e62d2480cefc16069436da95c1efb04
yolanda <yolanda93h@gmail.com>
2016-10-26 10:08:55 +0200

    this closes #2

--- src/main/scala/spamFilter.scala
+++ src/main/scala/spamFilter.scala
@@ -2,6 +2,7 @@
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql
 
 object spamFilter {
 
@@ -10,20 +11,20 @@ object spamFilter {
     println("Call to probaWordDir with args" + filesDir)
     // This function reads all the text files within a directory
     // wholeTextFiles get back a pair RDD where the key is the name of the input file.
-    val files  = sc.wholeTextFiles("/" + filesDir + "/*.txt").collect.toList.map(x => x._1)
+    val filesText  = sc.wholeTextFiles(filesDir + "/*/*.txt").collect.toList
     // The number of files is counted and stored in a variable nbFiles
-    val nbFiles = files.size
+    val nbFiles = filesText.size
     // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
     // toSet is used to remove duplicate words. listOfWords is the set of unique words
-    var listOfWords = files.flatMap(x => (sc.textFile(x).flatMap(_.split("\\s+"))).collect.toSet.toList).toSet
+    val listOfWords : Set[String] = filesText.flatMap(x =>x._2.split("\\s+")).toSet - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
     // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
-    listOfWords = listOfWords - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
     // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
-    def dfCalc(t:String, D: List[String]): Int = D.flatMap(x => sc.textFile(x).flatMap(_.split("\\s+")).filter(x => x==t).collect.toSet).count(x => x.size>0)
+    def dfCalc(t:String, D: List[(String,String)]): Int = D.map(x => x._2.split("\\s+").filter(x => x==t)).count(x => x.nonEmpty)
     // map structure: word => number of occurrences (files) of this word
-    val probaWord :RDD[(String, Double)] =sc.parallelize(listOfWords.map(x => (x, (dfCalc(x, files) / nbFiles).toDouble)).toSeq)
+    val probaWord  =sc.parallelize(listOfWords.map(x => (x, dfCalc(x, filesText).toDouble / nbFiles)).toSeq)
     (probaWord, nbFiles)
   }
+
 
   def computeMutualInformationFactor(
     probaWC:RDD[(String, Double)],
@@ -31,9 +32,8 @@ object spamFilter {
     probaC: Double,
     probaDefault: Double // default value when a probability is missing
   ):RDD[(String, Double)] = {
-  // Code to complete..
-
-
+    var probWJoin = probaW.fullOuterJoin(probaWC)
+    probaWC.map(x=>(x._1,x._2*math.log(x._2/x._3*probaC)))
   }
 
 
@@ -48,13 +48,23 @@ object spamFilter {
     val sc = new SparkContext(conf)
     val (probaW, nbFiles)= probaWordDir(sc)(args(0))
     // Compute the couples (probaWordHam, nbFilesHam) for the directory “ham” and (probaWordSpam, nbFilesSpam) for the directory “spam”
-    val (probaWordH,nbFilesH) = (probaWordDir(sc)(args(0).concat( "/" + "ham")))
-    val (probaWordS,nbFilesS) = (probaWordDir(sc)(args(0).concat( "/" + "spam")))
+    val (probaWordH,nbFilesH) = probaWordDir(sc)(args(0).concat( "/" + "ham"))
+    val (probaWordS,nbFilesS) = probaWordDir(sc)(args(0).concat( "/" + "spam"))
     //Compute the probability P(occurs, class) for each word.
-    val probaWC = (probaWordH,probaWordS,probaWordH.map(x => (x._1,1-x._2)),probaWordS.map(x => (x._1,1-x._2)))
     // RDD with the map structure: word => probability the word occurs (or not) in an email of a given class.
-    val probaC  // the probability that an email belongs to the given class.
-    computeMutualInformationFactor(probaWC,probaW,probaC,10) // the last is a default value
+    val probaWC = (probaWordH,probaWordS,probaWordH.map(x => (x._1,1-x._2)),probaWordS.map(x => (x._1,1-x._2)))
+    val probaH = nbFilesH/nbFiles // the probability that an email belongs to the given class.
+    val probaS = nbFilesS/nbFiles
+    // Compute mutual information for each class and occurs
+    var mutualInfH = computeMutualInformationFactor(probaWC._1,probaW,probaH,0.2/nbFiles) // the last is a default value
+    var mutualInfS = computeMutualInformationFactor(probaWC._2,probaW,probaS,0.2/nbFiles)
+    var mutualInfNh = computeMutualInformationFactor(probaWC._3,probaW,probaH,0.2/nbFiles)
+    var mutualInfNs = computeMutualInformationFactor(probaWC._4,probaW,probaS,0.2/nbFiles)
+
+    /*var listOfWords = extractListOfWords(sc)(args(0))
+    var mutualInfoW : RDD[(String,Double)]= listOfWords.map(x=>)*/
+
+    //These words must be also stored on HDFS in the file “/tmp/topWords.txt”.
 
   }
 
