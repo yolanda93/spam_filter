88c676fadcab080c4e2f305407bb4647fcf6cd2a
yolanda <yolanda93h@gmail.com>
2016-10-27 22:26:03 +0200

    some minor changes

--- src/main/scala/spamFilter.scala
+++ src/main/scala/spamFilter.scala
@@ -6,23 +6,23 @@ import org.apache.spark.SparkContext
 object spamFilter {
 
 
-  def probaWordDir(sc:SparkContext)(filesDir:String) :(RDD[(String, Double)], Long) = {
-    println("Call to probaWordDir with args" + filesDir)
-    // This function reads all the text files within a directory
-    // wholeTextFiles get back a pair RDD where the key is the name of the input file.
-    val filesText  = sc.wholeTextFiles(filesDir + "/*/*.txt").collect.toList
-    // The number of files is counted and stored in a variable nbFiles
-    val nbFiles = filesText.size
-    // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
-    // toSet is used to remove duplicate words. listOfWords is the set of unique words
-    val listOfWords : Set[String] = filesText.flatMap(x =>x._2.split("\\s+")).toSet - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
-    // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
-    // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
-    def dfCalc(t:String, D: List[(String,String)]): Int = D.map(x => x._2.split("\\s+").filter(x => x==t)).count(x => x.nonEmpty)
-    // map structure: word => number of occurrences (files) of this word
-    val probaWord  =sc.parallelize(listOfWords.map(x => (x, dfCalc(x, filesText).toDouble / nbFiles)).toSeq)
-    (probaWord, nbFiles)
-  }
+    def probaWordDir(sc:SparkContext)(filesDir:String) :(RDD[(String, Double)], Long) = {
+      println("Call to probaWordDir with args" + filesDir)
+      // This function reads all the text files within a directory
+      // wholeTextFiles get back a pair RDD where the key is the name of the input file.
+      val filesText  = sc.wholeTextFiles(filesDir).collect.toList
+      // The number of files is counted and stored in a variable nbFiles
+      val nbFiles = filesText.size
+      // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
+      // toSet is used to remove duplicate words. listOfWords is the set of unique words
+      val listOfWords : Set[String] = filesText.flatMap(x =>x._2.split("\\s+")).toSet - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
+      // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
+      // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
+      def dfCalc(t:String, D: List[(String,String)]): Int = D.map(x => x._2.split("\\s+").filter(x => x==t)).count(x => x.nonEmpty)
+      // map structure: word => number of occurrences (files) of this word
+      val probaWord  =sc.parallelize(listOfWords.map(x => (x, dfCalc(x, filesText).toDouble / nbFiles)).toSeq)
+      (probaWord, nbFiles)
+    }
 
   def computeMutualInformationFactor(
     probaWC:RDD[(String, Double)],
@@ -39,15 +39,16 @@ object spamFilter {
   def main(args: Array[String]) {
     if(args.length>0)
       println("Started Spam Filter with arg = " + args(0))
-      println("You should provide a directory path ")
+    else
+      println("You should provide the main directory path ")
 
     //val conf = new SparkConf().setAppName("Spam Filter Application")
     val conf = new SparkConf().setMaster("local").setAppName("Spam Filter Application")
     val sc = new SparkContext(conf)
-    val (probaW, nbFiles)= probaWordDir(sc)(args(0))
+    val (probaW, nbFiles)= probaWordDir(sc)(args(0) + "/*/*.txt")
     // Compute the couples (probaWordHam, nbFilesHam) for the directory “ham” and (probaWordSpam, nbFilesSpam) for the directory “spam”
-    val (probaWordH,nbFilesH) = probaWordDir(sc)(args(0).concat( "/" + "ham"))
-    val (probaWordS,nbFilesS) = probaWordDir(sc)(args(0).concat( "/" + "spam"))
+    val (probaWordH,nbFilesH) = probaWordDir(sc)(args(0).concat("/ham/*.txt"))
+    val (probaWordS,nbFilesS) = probaWordDir(sc)(args(0).concat("/spam/*.txt"))
     //Compute the probability P(occurs, class) for each word.
     // RDD with the map structure: word => probability the word occurs (or not) in an email of a given class.
     val probaWC = (probaWordH,probaWordS,probaWordH.map(x => (x._1,1-x._2)),probaWordS.map(x => (x._1,1-x._2)))
@@ -68,7 +69,52 @@ object spamFilter {
 
 } // end of spamFilter 
 
+/*
+Testing commands for spark-shell
+      var  filesDir = "/home/yolanda/workspace/spam_filter/ling-spam" + "/*/*.txt"
+      val filesText  = sc.wholeTextFiles(filesDir).collect.toList
+      // The number of files is counted and stored in a variable nbFiles
+      val nbFiles = filesText.size
+      // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
+      // toSet is used to remove duplicate words. listOfWords is the set of unique words
+      val listOfWords : Set[String] = filesText.flatMap(x =>x._2.split("\\s+")).toSet - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
+      // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
+      // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
+      def dfCalc(t:String, D: List[(String,String)]): Int = D.map(x => x._2.split("\\s+").filter(x => x==t)).count(x => x.nonEmpty)
+      // map structure: word => number of occurrences (files) of this word
+      val probaWord  =sc.parallelize(listOfWords.map(x => (x, dfCalc(x, filesText).toDouble / nbFiles)).toSeq)
+
+      var  filesDir = "/home/yolanda/workspace/spam_filter/ling-spam" + "/ham/*.txt"
+      val filesText  = sc.wholeTextFiles(filesDir).collect.toList
+      // The number of files is counted and stored in a variable nbFiles
+      val nbFiles = filesText.size
+      // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
+      // toSet is used to remove duplicate words. listOfWords is the set of unique words
+      val listOfWords : Set[String] = filesText.flatMap(x =>x._2.split("\\s+")).toSet - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
+      // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
+      // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
+      def dfCalc(t:String, D: List[(String,String)]): Int = D.map(x => x._2.split("\\s+").filter(x => x==t)).count(x => x.nonEmpty)
+      // map structure: word => number of occurrences (files) of this word
+      val probaWordH  =sc.parallelize(listOfWords.map(x => (x, dfCalc(x, filesText).toDouble / nbFiles)).toSeq)
+
+
+      var  filesDir = "/home/yolanda/workspace/spam_filter/ling-spam" + "/spam/*.txt"
+      val filesText  = sc.wholeTextFiles(filesDir).collect.toList
+      // The number of files is counted and stored in a variable nbFiles
+      val nbFiles = filesText.size
+      // Each text file must be splitted into a set of unique words (if a word occurs several times, it is saved only one time in the set).
+      // toSet is used to remove duplicate words. listOfWords is the set of unique words
+      val listOfWords : Set[String] = filesText.flatMap(x =>x._2.split("\\s+")).toSet - (".", ":", ",", " ", "/", "/", "-", "'","(", ")", "@")
+      // Non informative words must be removed from the set of unique words. The list of non-informative words is ".", ":", ",", " ", "/", "\", "-", "'", "(", ")", "@"
+      // read each file in fileStrings and for the word returns the number of files in which occurs in the directory
+      def dfCalc(t:String, D: List[(String,String)]): Int = D.map(x => x._2.split("\\s+").filter(x => x==t)).count(x => x.nonEmpty)
+      // map structure: word => number of occurrences (files) of this word
+      val probaWordS  =sc.parallelize(listOfWords.map(x => (x, dfCalc(x, filesText).toDouble / nbFiles)).toSeq)
+
+      val probaWC = (probaWordH,probaWordS,probaWordH.map(x => (x._1,1-x._2)),probaWordS.map(x => (x._1,1-x._2)))
+      val probaH = nbFilesH/nbFiles // the probability that an email belongs to the given class.
+      val probaS = nbFilesS/nbFiles
 
 
 
-
+ */
